72364a340a0582f75adca2b2f4820d7e42e664eb
Mohamed Ameziane <med.ameez02@gmail.com>
2025-05-05 16:29:41 +0100

    Login Activity update

--- app/src/main/java/ma/enset/quizapp/LoginActivity.java
+++ app/src/main/java/ma/enset/quizapp/LoginActivity.java
@@ -1,6 +1,13 @@
 package ma.enset.quizapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,19 +17,46 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText etEmail,etPassword;
+    Button bLogin;
+    TextView tvRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Changer le titre de la barre d'action
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setTitle("Connexion");
-        }
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        etEmail=findViewById(R.id.editTextEmail);
+        etPassword=findViewById(R.id.editTextPassword);
+        bLogin=findViewById(R.id.buttonLogin);
+        tvRegister=findViewById(R.id.textViewRegister);
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (etEmail.getText().toString().isEmpty() || etPassword.getText().toString().isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Veuillez remplir tous les champs.", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (etEmail.getText().toString().equals("test@gmail.com") &&
+                            etPassword.getText().toString().equals("123456789")
+                    ) {
+                        Toast.makeText(LoginActivity.this, "Connexion réussie", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(LoginActivity.this, QuestionActvity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Email ou mot de passe incorrect", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
         });
     }
 }
